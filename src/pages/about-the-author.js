345c84a136b3bda850e652465d4ab5022f234119
unknown <Justin>
2020-10-09 06:38:48 +0800

    changes-10-9-2020

--- src/pages/about-the-author.js
+++ src/pages/about-the-author.js
@@ -11,27 +11,29 @@ const ATB = (props)=>{
 		 	<Layout>
 		 		<Nav pathExt={props.path}/>
                  <Banner titlePage={`<span>About</span><span>the</span><span>Author</span>`}/>
-                  
-		 		<div className="container">
-				    <section className="body-author-contents columns">
-                         
-                        <div className="heading-quote column">
-                            <div className="author-image-container">
-                                <img src={AuthorImg} />
-                            </div>
-                        </div>
 
-                        <article className="article-section column" id="author">
-                            <p>
-                               PETER JOHNSTON HAS BEEN A FAMILY DOCTOR FOR FIFTY YEARS,
-                               HIS WIFE'S INCURABLE ILLNESS IN 1979 LED HIM  TO EXPLORE
-                               THE CONNECTION BETWEEN THE BODY, MIND AND CONSCIOUNSNESS.
-                               AS PIONEER OF HOLISTIC MEDICINE, HE HAS COMBINED ALTERNATIVE
-                               APPROACHES WITH ORTHODOX MEDICINE FOR FORTY YEARS.
-                            </p>
-                        </article>
-                    </section> 
-                </div>          
+                <section className="section-about-author">
+                    <div className="container">
+                        <section className="body-author-contents ">
+                            
+                            <div className="heading-quote">
+                                <div className="author-image-container">
+                                    <img src={AuthorImg} />
+                                </div>
+                            </div>
+
+                            <article className="article-section" id="author">
+                                <p>
+                                PETER JOHNSTON HAS BEEN A FAMILY DOCTOR FOR FIFTY YEARS,
+                                HIS WIFE'S INCURABLE ILLNESS IN 1979 LED HIM  TO EXPLORE
+                                THE CONNECTION BETWEEN THE BODY, MIND AND CONSCIOUNSNESS.
+                                AS PIONEER OF HOLISTIC MEDICINE, HE HAS COMBINED ALTERNATIVE
+                                APPROACHES WITH ORTHODOX MEDICINE FOR FORTY YEARS.
+                                </p>
+                            </article>
+                        </section> 
+                    </div>
+                </section>  
 
 		 	</Layout>
 		</>
